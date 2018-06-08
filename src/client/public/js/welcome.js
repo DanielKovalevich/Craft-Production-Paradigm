9ded7898462a661f6c02962614e679f5c20423e6
DanielKovalevich <dkoval526@gmail.com>
2018-06-07 22:00:27 -0400

    Added content to Modals

--- src/client/public/js/welcome.js
+++ src/client/public/js/welcome.js
@@ -1,11 +1,25 @@
 $(document).ready(() => {
   $('#new-game').click((e) => {
     e.preventDefault();
-    $('#new-game-modal').modal('show');    
+    $('#new-game-modal').modal({
+      onApprove : function() {
+        // TODO: Validation and call to server
+        //... //Validate here, or pass validation to somewhere else
+        return false; //Return false as to not close modal dialog
+      }
+    }).modal('show');    
   });
 
   $('#join-game').click((e) => {
     e.preventDefault();
-    $('#join-game-modal').modal('show');
+    $('#join-game-modal').modal({
+      onApprove : function() {
+        // TODO: Validation and call to server
+        //... //Validate here, or pass validation to somewhere else
+        return false; //Return false as to not close modal dialog
+      }
+    }).modal('show');    
   });
+
+  $('.ui.dropdown').dropdown();
 });
