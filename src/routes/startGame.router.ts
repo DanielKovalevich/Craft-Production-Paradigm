8bd4ebf863e1bd9859a2dbd0400ecf84cd5cb3f1
DanielKovalevich <dkoval526@gmail.com>
2018-06-18 15:44:18 -0400

    Made sure to have fewer Database Connections

--- src/routes/startGame.router.ts
+++ src/routes/startGame.router.ts
@@ -3,30 +3,33 @@ import {GameController} from '../controllers/GameController'
 import * as cors from 'cors';
 
 const router: Router = Router();
+const controller: GameController = new GameController();
 
 router.post('/', cors(), (req: Request, res: Response) => {
-  let game: GameController = new GameController();
   let result: any = {"pin" : 0};
-  result.pin = game.addNewGame(req);
+  result.pin = controller.addNewGame(req);
   res.send(result);
 });
 
 router.get('/getGameInfo/:id', cors(), async (req: Request, res: Response) => {
-  let game = new GameController();
-  res.send(await game.getGameInfo(req.params.id));
+  res.send(await controller.getGameInfo(req.params.id));
 });
 
 router.get('/addActivePlayer/:id', cors(), (req: Request, res: Response) => {
-  let game = new GameController();
-  game.addActivePlayer(req.params.id);
+  controller.addActivePlayer(req.params.id);
   res.send(200);
 });
 
 router.get('/removeActivePlayer/:id', cors(), (req: Request, res: Response) => {
-  let game = new GameController();
-  game.removeActivePlayer(req.params.id);
+  controller.removeActivePlayer(req.params.id);
   res.send(200);
-})
+});
+
+router.get('/checkIfPinExists/:id', cors(), (req: Request, res: Response) => {
+  controller.checkIfPinExists(req.params.id, (result:any) => {
+    res.send(result);
+  });
+});
 
 // if I create self-contained functions, I can write them like this
 //router.post('/', new GameController().addNewGame);
