dd82c7f81af982b094a6f299e5f228c631a71cb5
DanielKovalevich <dkoval526@gmail.com>
2018-06-29 19:36:04 -0400

    Starting to add ability to see orders

--- src/client/public/js/legoBuilder/pageScripts.js
+++ src/client/public/js/legoBuilder/pageScripts.js
@@ -1,3 +1,11 @@
+$(document).ready(() => {
+  $('#order').click(e => {
+    checkOrders();
+    //$('#no-orders').modal('show');
+    $('#ready-order').modal('show');
+  });
+});
+
 /**
  * ================================================================
  *                    HTML BUTTOM FUNCTIONS
@@ -8,11 +16,15 @@ function cycle() {
   let index = allModels.indexOf(currentObj);
   currentObj = ++index == allModels.length ? allModels[0] : allModels[index];
   loadRollOverMesh();
- }
+}
 
- function getModel(name) {
-   allModels.forEach((element) => {
-    if (element.name == name) currentObj = element;
-   });
-   loadRollOverMesh();
- }
+function getModel(name) {
+  allModels.forEach((element) => {
+  if (element.name == name) currentObj = element;
+  });
+  loadRollOverMesh();
+}
+
+function checkOrders() {
+
+}
