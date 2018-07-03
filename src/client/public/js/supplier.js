c9ec0928123bc0983b01fecfde598c453406d7e0
DanielKovalevich <dkoval526@gmail.com>
2018-07-03 13:53:47 -0400

    Buttons on supplier page affect supply order

--- src/client/public/js/supplier.js
+++ src/client/public/js/supplier.js
@@ -4,12 +4,39 @@ const names = ["1x1", "2x2", "2x3x2", "1x2 Pin",
               "Rim 3", "1x2", "1x4", "1x2 Plate",
               "4x6 Plate", "6x8 Plate", "2x10 Plate", "Windshield",
               "Steering Wheel", "Lego Man"];
+let pieceOrders = [];
 
 $(document).ready(() => {
   generateSupplyGrid();
-  
+  initArray();
+  initButtons();
 });
 
+function initArray() {
+  for (let i = 0; i < names.length; i++) pieceOrders[i] = 0;
+}
+
+function initButtons() {
+  for (let i = 0; i < names.length; i++) {
+    let num = '#' + i;
+    $(num + '-plus').click(e => {
+      let currentNum = parseInt($(num + '-value').html());
+      $(num + '-value').html(currentNum < 10 ? ++currentNum : 10);
+      pieceOrders[i] = currentNum;
+    });
+    $(num + '-minus').click(e => {
+      let currentNum = parseInt($(num + '-value').html());
+      $(num + '-value').html(currentNum == 0 ? 0 : --currentNum);
+      pieceOrders[i] = currentNum;
+      console.log(pieceOrders);
+    });
+  }
+}
+
+/**
+ * Dynamically generate all the squares to add to a supply order
+ * This would have been terrible to do by hand
+ */
 function generateSupplyGrid() {
   let html = "";
   for (let i = 0; i < names.length / 4; i++) {
@@ -18,12 +45,15 @@ function generateSupplyGrid() {
       if (i * 4 + j < names.length) {
         html += '<div class="four wide column">';
         html += '<p>' + names[i * 4 + j] + '</p>';
-        html += '<div class="row">'
-        html += '<div class="ui icon buttons">]' +
-          '<button class="ui button"><i class="pause icon"></i></button>' +
-          '<button class="ui button"><i class="shuffle icon"></i></button></div></div></div>';
+        // Start off each piece with an order of 0
+        html += '<div class="row"><div class="ui statistic"><div id="' + (i * 4 + j) + '-value' + '"class="value">0</div></div></div>'
+        // Adds the plus and minus buttons to each piece
+        html += '<div class="row"><div class="ui icon buttons">' +
+          '<button id="'+ (i * 4 + j) + '-minus' + '" class="ui button"><i class="minus icon"></i></button>' +
+          '<button id="'+ (i * 4 + j) + '-plus' + '" class="ui button"><i class="plus icon"></i></button></div></div></div>';
       }
     }
+    // I want there to be vertical lines between each cube so I need to add a blank space 
     if (i + 1 >= names.length / 4) html += '<div class="five wide column"></div>';
     html += '</div>';
     $('#supply-grid').append(html);
