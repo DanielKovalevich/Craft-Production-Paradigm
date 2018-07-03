ae24376360402dd6a6e7a8a5cd142cdea7c974b8
DanielKovalevich <dkoval526@gmail.com>
2018-07-03 16:41:11 -0400

    Added ability to see orders on the supplier page

--- src/client/public/js/supplier.js
+++ src/client/public/js/supplier.js
@@ -5,12 +5,23 @@ const names = ["1x1", "2x2", "2x3x2", "1x2 Pin",
               "4x6 Plate", "6x8 Plate", "2x10 Plate", "Windshield",
               "Steering Wheel", "Lego Man"];
 let pieceOrders = [];
+orderInformation = {};
+currentOrder = {};
 
 $(document).ready(() => {
   generateSupplyGrid();
   initArray();
   initButtons();
+  $('#order').click(e => {
+    openModal();
+  });
+  checkOrders();
 });
+
+// gets the pin from the url
+function getPin() {
+  return /(\d+)(?!.*\d)/g.exec(window.location.href)[0];
+}
 
 function initArray() {
   for (let i = 0; i < names.length; i++) pieceOrders[i] = 0;
@@ -31,6 +42,62 @@ function initButtons() {
       console.log(pieceOrders);
     });
   }
+  $('#left').click(e => {
+    let index = orderInformation.indexOf(currentOrder);
+    currentOrder = --index < 0 ? orderInformation[orderInformation.length - 1] : orderInformation[index];
+    updateOrder();
+  });
+  $('#right').click(e => {
+    let index = orderInformation.indexOf(currentOrder);
+    currentOrder = ++index == orderInformation.length ? orderInformation[0] : orderInformation[index];
+    updateOrder();
+  });
+}
+
+function openModal() {
+  if (jQuery.isEmptyObject(orderInformation)) {
+    $('#no-orders').modal('show');
+  }
+  else {
+    $('#ready-order').modal('show');
+  }
+}
+
+function updateOrder() {
+  switch(currentOrder.modelType) {
+    case 'super': $('#order-image').attr('src', '/../images/race.jpg');        break;
+    case 'race': $('#order-image').attr('src', '/../images/lego_car.jpg');     break;
+    case 'RC': $('#order-image').attr('src', '/../images/rc.jpg');             break;
+    case 'yellow': $('#order-image').attr('src', '/../images/yellow_car.jpg'); break;
+  }
+  console.log(currentOrder);
+  let html = '<p>Date Ordered: ' + new Date(currentOrder.createDate).toString() + '</p>';
+  html += '<p>Model Type: ' + currentOrder.modelType + '</p>';
+  html += '<p>Status: ' + currentOrder.status + '</p><br>';
+  $('#order-info').html(html);
+}
+
+function checkOrders() {
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:3000/gameLogic/getOrders/' + getPin(),
+    timeout: 5000,
+    success: (data) => {
+      if (orderInformation.length != data.length) {
+        orderInformation = data;
+        // Need to find the oldest order that hasn't been finished or canceled
+        let i = 0;
+        while(orderInformation[i].status != 'In Progress') i++;
+        currentOrder = orderInformation[i];
+        updateOrder();
+      }
+    },
+    error: (xhr, status, error) => {
+      console.log('Error: ' + error);
+    }
+  });
+
+  setTimeout(checkOrders, 3000);
 }
 
 /**
