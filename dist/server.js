c7c24de72e42c617744477ad3290e99d32de38c1
DanielKovalevich <dkoval526@gmail.com>
2018-07-25 13:20:39 -0400

    Heroku apparently doesn't like compiling my code

--- dist/server.js
+++ dist/server.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /* app/server.ts */
 require('dotenv').config();
 const app_1 = require("./models/app");
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
 app_1.default.set('port', port);
 app_1.default.listen(app_1.default.get('port'), () => {
-    console.log(`Listening at http://localhost:${port}/`);
+    console.log(`Listening at http://psu-research-api.heroku.com:${port}/`);
 });
